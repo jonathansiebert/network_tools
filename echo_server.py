43098017b142ccbcbd80b4db9ae8bd70b61de2c2
lfritts <llfritts@gmail.com>
2014-06-09 15:22:39 -0700

    Add empty files

---
+++ echo_server.py
@@ -0,0 +1,14 @@
+#! /usr/local/bin/python
+
+import socket
+
+def echo_server():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM,
+                                  socket.IPPROTO_IP)
+    server_socket.bind(('127.0.0.1',50000))
+    server_socket.listen(1)
+    conn, addr = server_socket.accept()
+
+
+if __name__ == "__main__":
+    echo_server()
